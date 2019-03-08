076b82c23f45b4d9387baefd74e00b153f458c11
Reybel C <reybel24@gmail.com>
2019-03-08 02:18:28 -0500

    add filters for price, flavor, and contains nuts

--- productList.js
+++ productList.js
@@ -2,25 +2,27 @@
 
     // "Database" of candies (but really just an array)
     var mockDatabase = [
-        { _id: 'skittles_original', name: 'Skittles', price: '5.00', description: 'These are skittles.', brand: 'Wrigley Company', year: '1974', flavor: 'sweet' },
-        { _id: 'starbursts_original', name: 'Starbursts', price: '10.00', description: 'These are starbursts.', brand: 'Wrigley Company', year: '1960', flavor: 'sweet' },
-        { _id: 'smarties', name: 'Smarties', price: '9.99', description: 'These are smarties.', brand: 'Smarties Candy Company', year: '1949', flavor: 'sweet' },
-        { _id: 'hersheys_original', name: 'Hersheys', price: '14.99', description: 'These are hersheys.', brand: 'The Hershey Company', year: '1935', flavor: 'sweet' },
-        { _id: 'kitkat_original', name: 'Kitkat', price: '10.99', description: 'These are hersheys.', brand: 'Nestlé', year: '1935', flavor: 'sweet' },
-        { _id: 'hersheys_cookiesncream', name: 'Hersheys Cookies N Cream', price: '10.99', description: 'These are hersheys.', brand: 'Nestlé', year: '1935', flavor: 'sweet' },
-        { _id: 'twizzlers_original', name: 'Twizzlers', price: '1.99', description: 'These are twizzlers.', brand: 'The Hershey Company', year: '1845', flavor: 'sweet' },
-        { _id: 'mentos_mint', name: 'Mentos', price: '10.00', description: 'These are starbursts.', brand: 'Other', year: '1932', flavor: 'mint' },
-        { _id: 'airheads', name: 'AirHeads Xtremes Bites', price: '14.99', description: 'These are hersheys.', brand: 'Other', year: '1985', flavor: 'sweet' },
-        { _id: 'york_peppermint', name: 'York Peppermint Patties', price: '8.99', description: 'These are hersheys.',brand: 'The Hershey Company', year: '1975', flavor: 'mint' },
-        { _id: 'reeses_pieces', name: 'Resses Pieces', price: '11.99', description: 'These are hersheys.', brand: 'The Hershey Company', year: '1978', flavor: 'sweet' },
-        { _id: 'milky_way', name: 'Milky Way', price: '14.99', description: 'These are hersheys.', brand: 'Mars', year: '1923', flavor: 'sweet' },
-        { _id: 'twix', name: 'Twix', price: '16.99', description: 'These are hersheys.', brand: 'Mars', year: '1979', flavor: 'sweet' },
-        { _id: 'sour_patch_kids', name: 'Sour Patch Kids', price: '11.99', description: 'These are hersheys.', brand: 'Mars', year: '1985', flavor: 'sour' },
-        { _id: 'now_and_later', name: 'Now and Later Extreme Sour', price: '4.99', description: 'These are hersheys.', brand: 'Other', year: '1962', flavor: 'sour' },
-        { _id: 'trolli', name: 'Trolli Sour Brite Crawlers', price: '9.99', description: 'These are hersheys.', brand: 'Other', year: '1981', flavor: 'sour' },
-        { _id: 'starlight_mints', name: 'Starlight Mints', price: '5.99', description: 'These are hersheys.', brand: 'Other', year: '2001', flavor: 'mint' },
+        { _id: 'skittles_original', name: 'Skittles', price: '5.00', description: 'These are skittles.', brand: 'Wrigley Company', year: '1974', flavor: 'sweet', nuts: 'no' },
+        { _id: 'starbursts_original', name: 'Starbursts', price: '10.00', description: 'These are starbursts.', brand: 'Wrigley Company', year: '1960', flavor: 'sweet', nuts: 'no' },
+        { _id: 'smarties', name: 'Smarties', price: '9.99', description: 'These are smarties.', brand: 'Smarties Candy Company', year: '1949', flavor: 'sweet', nuts: 'no' },
+        { _id: 'hersheys_original', name: 'Hersheys', price: '14.99', description: 'These are hersheys.', brand: 'The Hershey Company', year: '1935', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'kitkat_original', name: 'Kitkat', price: '10.99', description: 'These are hersheys.', brand: 'Nestle', year: '1935', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'hersheys_cookiesncream', name: 'Hersheys Cookies N Cream', price: '10.99', description: 'These are hersheys.', brand: 'Nestlé', year: '1935', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'twizzlers_original', name: 'Twizzlers', price: '1.99', description: 'These are twizzlers.', brand: 'The Hershey Company', year: '1845', flavor: 'sweet', nuts: 'no' },
+        { _id: 'mentos_mint', name: 'Mentos', price: '10.00', description: 'These are starbursts.', brand: 'Other', year: '1932', flavor: 'mint', nuts: 'no' },
+        { _id: 'airheads', name: 'AirHeads Xtremes Bites', price: '14.99', description: 'These are hersheys.', brand: 'Other', year: '1985', flavor: 'sweet', nuts: 'no' },
+        { _id: 'york_peppermint', name: 'York Peppermint Patties', price: '8.99', description: 'These are hersheys.',brand: 'The Hershey Company', year: '1975', flavor: 'mint', nuts: 'no' },
+        { _id: 'reeses_pieces', name: 'Resses Pieces', price: '11.99', description: 'These are hersheys.', brand: 'The Hershey Company', year: '1978', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'milky_way', name: 'Milky Way', price: '14.99', description: 'These are hersheys.', brand: 'Mars', year: '1923', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'twix', name: 'Twix', price: '16.99', description: 'These are hersheys.', brand: 'Mars', year: '1979', flavor: 'sweet', nuts: 'yes' },
+        { _id: 'sour_patch_kids', name: 'Sour Patch Kids', price: '11.99', description: 'These are hersheys.', brand: 'Mars', year: '1985', flavor: 'sour', nuts: 'no' },
+        { _id: 'now_and_later', name: 'Now and Later Extreme Sour', price: '4.99', description: 'These are hersheys.', brand: 'Other', year: '1962', flavor: 'sour', nuts: 'no' },
+        { _id: 'trolli', name: 'Trolli Sour Brite Crawlers', price: '9.99', description: 'These are hersheys.', brand: 'Other', year: '1981', flavor: 'sour', nuts: 'no' },
+        { _id: 'starlight_mints', name: 'Starlight Mints', price: '5.99', description: 'These are hersheys.', brand: 'Other', year: '2001', flavor: 'mint', nuts: 'no' },
     ];
 
+    // 'Filtering by' text
+    var text_filteringBy = document.querySelector('#top-message');
 
     // This function renders the products individually on the page given
     // a list of results
@@ -51,7 +53,7 @@
 
     // This will order results list
     function orderBy(sortValue) {
-        console.log("SORTING: " + sortValue);
+        text_filteringBy.innerHTML = "Sorting by " + sortValue;
         // Sort method varies based on what type of value we're sorting
         var sortedResults = (sortValue === 'name') ?
             mockDatabase.sort(function (a, b) { // Strings need to be sorted in a slightly more complex way
@@ -82,11 +84,12 @@
     });
 
 
+    //FILTER 1
     // This function filters the candies by brand
     function filterByBrand(brand) {
+        text_filteringBy.innerHTML = "Filtering by " + brand;
         var filteredResults = mockDatabase.filter(function (result) {
             // If this candy matches the brand, return TRUE
-            console.log(result.brand + " and " + brand);
             return (result.brand == brand);
         });
         renderList(filteredResults);
@@ -96,6 +99,58 @@
         var brand = event.target.value;
         filterByBrand(brand);
     });
+
+
+    //FILTER 2
+    // This function filters the candies by whether or not they contain nuts
+    function filterByPrice(priceRange) {
+        text_filteringBy.innerHTML = "Filtering by price (" + priceRange + ")";
+        var filteredResults = mockDatabase.filter(function (result) {
+            // If this candy matches the brand, return TRUE
+            return (result.price <= priceRange);
+        });
+        renderList(filteredResults);
+    }
+    // Value of the input has changed, re-run the filter function
+    document.querySelector('#filterByPrice').addEventListener('change', function(event){
+        var priceRange = event.target.value;
+        filterByPrice(priceRange);
+    });
+
+
+    //FILTER 3
+    // This function filters the candies by flavor
+    function filterByFlavor(flavor) {
+        text_filteringBy.innerHTML = "Filtering by " + flavor;
+        var filteredResults = mockDatabase.filter(function (result) {
+            // If this candy matches the brand, return TRUE
+            return (result.flavor == flavor);
+        });
+        renderList(filteredResults);
+    }
+    // Value of the input has changed, re-run the filter function
+    document.querySelector('#filterByFlavor').addEventListener('change', function(event){
+        var flavor = event.target.value;
+        filterByFlavor(flavor);
+    });
+
+
+    //FILTER 4
+    // This function filters the candies by whether or not they contain nuts
+    function filterByNuts(containsNuts) {
+        text_filteringBy.innerHTML = (containsNuts == "yes") ?  "These candies contain nuts" : "These candies do not contain nuts";
+        var filteredResults = mockDatabase.filter(function (result) {
+            return (result.nuts == containsNuts);
+        });
+        renderList(filteredResults);
+    }
+    // Value of the input has changed, re-run the filter function
+    document.querySelector('#filterByContainsNuts').addEventListener('change', function(event){
+        var containsNutes = event.target.value;
+        filterByNuts(containsNutes);
+    });
+
+
 
     function setSelectedProduct(productID)
     {
